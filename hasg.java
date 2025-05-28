8d1962cabde729b9a15d68ff408cb6da41688943
aarchi2005 <aarchilahariya@gamil.com>
2025-05-28 15:27:47 +0530

    hello

---
+++ hasg.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class hasg {
+    public static void main(String[] args) {
+        String str ="aaabcccdddeeef";
+
+        HashMap<String, Integer> map =new HashMap<>();
+for ( str)
+    }
+}
--- hashm.java
+++ hashm.java
@@ -1,11 +1,11 @@
 import java .util.HashMap;
 public class hashm {
     public static void main(String[] args) {
-        String str ="prograaming";
-        char [] ch = str.toCharArray();
-        HashMap<Character ,Integer>map=new HashMap<>();
-      for (char i :ch){
-        map.put(i,map.getOrDefault(i, 0)+1);
+        String str1 ="prograaming is a fun and fun is a prograaming";
+       String [] newStr = str1.split(" ");
+        HashMap<String ,Integer>map=new HashMap<>();
+      for (String str : newStr){
+        map.put(str,map.getOrDefault(str , 0)+1);
       }
 System.out.println(map);
 
