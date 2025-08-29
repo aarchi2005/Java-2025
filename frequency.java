2464fe9c55458fb5ac78106dc709a4bd56fdb896
aarchi2005 <aarchilahariya@gamil.com>
2025-08-29 22:58:38 +0530

    hii

--- frequency.java
+++ frequency.java
@@ -1,8 +1,8 @@
  import java.util.HashMap;
 public class frequency {
     public static void main(String[] args) {
-        String str2 = " e l l h o";
-        String str1 ="h e l l o ";
+        String str2 = "e l l h o";
+        String str1 ="h e l l o";
        String [] newStr = str1.split(" ");
        String [] newstr =str2.split(" ");
         HashMap<String ,Integer>map=new HashMap<>();
@@ -11,6 +11,12 @@ public class frequency {
       for (String str : newStr){
         map.put(str,map.getOrDefault(str , 0)+1);
         fre.put(str , fre.getOrDefault(str ,0 )+1);
+      }
+      if(str1.length()==str2.length()){
+System.out.println("true");
+      }
+      else{
+        System.out.println("false ");
       }
 System.out.println(map);
 System.out.println(fre);
